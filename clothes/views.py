a9720dd7effb452383c56c6ef0702c771950a868
fergalquinn77 <fergal@bourbon.ie>
2022-08-20 10:38:03 +0000

    Started clothes template

--- clothes/views.py
+++ clothes/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Clothes
 
 # Create your views here.
+
+def all_clothes(request):
+    """ View to return all clothes, including sorting and search queries"""
+
+    clothes = Clothes.objects.all()
+    context = {
+        'clothes': clothes,
+    }
+    return render(request, 'clothes/clothes.html', context)
