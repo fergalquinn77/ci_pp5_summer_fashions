cf6ea5111e8d6733294541b796b816492fa8eec1
fergalquinn77 <fergal@bourbon.ie>
2022-08-21 20:44:38 +0000

    Create skeleton item detail page

--- clothes/urls.py
+++ clothes/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.all_clothes, name='clothes'),
+    path('<item_id>/', views.item_details, name='item_details'),
 ]
--- clothes/views.py
+++ clothes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Clothes
 
 # Create your views here.
@@ -11,3 +11,12 @@ def all_clothes(request):
         'clothes': clothes,
     }
     return render(request, 'clothes/clothes.html', context)
+
+def item_details(request, item_id):
+    """ View to return detail on an item of clothes"""
+
+    item = get_object_or_404(Clothes, pk=item_id)
+    context = {
+        'item': item,
+    }
+    return render(request, 'clothes/item_details.html', context)
