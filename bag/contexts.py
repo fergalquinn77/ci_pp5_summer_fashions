a74bc827051dba8b15d43c040977085ab2d151d9
fergalquinn77 <fergal@bourbon.ie>
2022-08-26 17:24:31 +0000

    Update all items to has_sizes in Python Shell

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,10 +1,25 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from clothes.models import Clothes
+
 def bag_contents(request):
 
     bag_items = []
     total = 0
-    product_count = 0
+    item_count = 0
+    bag = request.session.get('bag', {})
+
+    for item_id, item_data in bag.items():
+        if isinstance(item_data, int):
+            item = get_object_or_404(Clothes, pk=item_id)
+            total += item_data * item.price
+            item_count += item_data
+            bag_items.append({
+                'item_id': item_id,
+                'quantity': item_data,
+                'item': item,
+            })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
@@ -17,7 +32,7 @@ def bag_contents(request):
     context = {
         'bag_items': bag_items,
         'total': total,
-        'product_count': product_count,
+        'item_count': item_count,
         'delivery': delivery,
         'free_delivery_delta': free_delivery_delta,
         'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
