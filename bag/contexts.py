c32014ee662fd59c0e9c17e785f7796b8392bacb
fergalquinn77 <fergal@bourbon.ie>
2022-08-28 13:58:44 +0000

    Set up shopping bag view and add product sizes

--- bag/contexts.py
+++ bag/contexts.py
@@ -21,6 +21,18 @@ def bag_contents(request):
                 'item': item,
             })
 
+        else:
+            product = get_object_or_404(Clothes, pk=item_id)
+            for size, quantity in item_data['items_by_size'].items():
+                total += quantity * product.price
+                item_count += quantity
+                bag_items.append({
+                    'item_id': item_id,
+                    'quantity': quantity,
+                    'product': product,
+                    'size': size,
+                })
+
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
         free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
