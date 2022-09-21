75ff72e3fd001a6d44e7918f591a06f496ef7fdf
fergalquinn77 <fergal@bourbon.ie>
2022-09-21 21:21:44 +0000

    Heroku deployment

--- summerfashions/settings.py
+++ summerfashions/settings.py
@@ -128,12 +128,12 @@ LOGIN_REDIRECT_URL = '/'
 WSGI_APPLICATION = 'summerfashions.wsgi.application'
 
 
-# if 'DATABASE_URL' in os.environ:
-#     DATABASES = {
-#         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-#     }
-# else:
-DATABASES = {
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
+    DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
