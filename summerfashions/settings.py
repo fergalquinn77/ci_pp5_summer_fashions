8f430d780ac62013f3779fb913a7497f7d13e214
fergalquinn77 <fergal@bourbon.ie>
2022-09-21 22:40:20 +0000

    Heroku Deploy

--- summerfashions/settings.py
+++ summerfashions/settings.py
@@ -23,6 +23,7 @@ if os.path.exists('env.py'):
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -128,12 +129,12 @@ LOGIN_REDIRECT_URL = '/'
 WSGI_APPLICATION = 'summerfashions.wsgi.application'
 
 
-if 'DATABASE_URL' in os.environ:
-    DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-    }
-else:
-    DATABASES = {
+# if 'DATABASE_URL' in os.environ:
+#     DATABASES = {
+#         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+#     }
+# else:
+DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
